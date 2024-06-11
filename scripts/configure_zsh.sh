0ca7fad65da71013af46b567de5452b245b83fd8
forketyfork <forketyfork@icloud.com>
2024-06-11 17:54:56 +0200

    configure zsh for linux

--- scripts/configure_zsh.sh
+++ scripts/configure_zsh.sh
@@ -10,3 +10,9 @@ chsh -s "$(which zsh)"
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# download the font
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/IosevkaTerm.zip
+cd ~/.local/share/fonts
+unzip JetBrainsMono.zip
+rm JetBrainsMono.zip
+fc-cache -fv
