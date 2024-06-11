aaf5346f1b84459b6a55896b0208ce885b948441
forketyfork <forketyfork@icloud.com>
2024-06-11 16:11:19 +0200

    Added haskell-vim bundle

--- scripts/install_pathogen.sh
+++ scripts/install_pathogen.sh
@@ -1,2 +1,21 @@
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+#!/bin/sh
+# Installation of vim-pathogen and vim bundles
+
+# Exit immediately on a non-zero exit code
+set -e
+
+# Install vim-pathogen
+echo "Creating required vim configuration directories"
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+echo "Downloading pathogen.vim"
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Install haskell-vim bundle
+haskell_vim_dir=~/.vim/bundle/haskell-vim
+if [ -d "$haskell_vim_dir" ]; then
+    echo "The $haskell_vim_dir directory exists, pulling"
+    git -C "$haskell_vim_dir" pull
+else
+    echo "The $haskell_vim_dir directory doesn't exist, cloning"
+    git clone https://github.com/neovimhaskell/haskell-vim.git "$haskell_vim_dir"
+fi
