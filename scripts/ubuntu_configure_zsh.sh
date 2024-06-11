d76c17ad4fd797d390736c6069021f187632a1c7
forketyfork <forketyfork@icloud.com>
2024-06-11 20:42:29 +0200

    zsh configuration for ubuntu

--- scripts/ubuntu_configure_zsh.sh
+++ scripts/ubuntu_configure_zsh.sh
@@ -7,10 +7,15 @@ set -eu
 echo "Setting zsh as the default shell"
 chsh -s "$(which zsh)"
 
-echo "Installing oh-my-zsh"
-ZSH="sh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d ~/.oh-my-zsh ]; then
+    echo "Installing oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "oh-my-zsh is already installed, skipping"
+fi
 
-# download the font
+# download the Iosevka font
+echo "Downloading and installing the IosevkaTerm font"
 wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/IosevkaTerm.zip
 cd ~/.local/share/fonts
 unzip JetBrainsMono.zip
