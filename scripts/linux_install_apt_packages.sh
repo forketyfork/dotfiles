defa0e8923936f39107b0d65091d3a8098f97f16
forketyfork <forketyfork@icloud.com>
2024-06-12 10:54:20 +0200

    Added conditional sudo installation

--- scripts/linux_install_apt_packages.sh
+++ scripts/linux_install_apt_packages.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 # Install required packages using apt
 # Tested on Ubuntu 22.04
 
 # Exit immediately on a non-zero exit code, fail on unknown variables
 set -eu
+
+# install sudo if absent
+command -v sudo &>/dev/null || (apt-get update && apt-get install sudo -y)
 
 sudo apt-get update
 sudo apt-get upgrade -y
